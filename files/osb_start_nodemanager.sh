716bd4c2315bc8ece2127e4f314901dfd9d773b0
abessifi <ahmed.bessifi@gmail.com>
2016-06-08 11:52:21 +0200

    [Docker] Add 'osb_start_nodemanager.sh' script to run NodeManager in foreground

---
+++ files/osb_start_nodemanager.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Set environement variables
+source ~/scripts/osb_set_environment_variables.sh
+
+export JAVA_OPTIONS=-Djava.security.egd=file:///dev/urandom
+export PATH=${DOMAIN_CONFIGURATION_HOME}/bin/:$PATH
+
+# Check if the Nodemanager is running and start it if not.
+if [ ! -f ${NODE_MANAGER_HOME}/nodemanager.process.lck ]; then
+    startNodeManager.sh
+else
+  echo "[INFO] The Nodemanager is already running."
+fi
