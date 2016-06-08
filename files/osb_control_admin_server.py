cd618866cb670cdb8c4097ef447faf908d9a6edc
abessifi <ahmed.bessifi@gmail.com>
2016-06-08 17:22:41 +0200

    Add Ansible tags to control Nodemanager and AdminServer status

--- files/osb_control_admin_server.py
+++ files/osb_control_admin_server.py
@@ -1,6 +1,11 @@
 import socket;
 import os.path;
+import sys;
 
+# Get control action from the command line
+control_action=sys.argv[1];
+# Set parameters from environment variables and the sourced property file passed
+# as argument to the script.
 print '[INFO] Setting parameters..';
 domain_application_home = os.getenv('DOMAIN_APPLICATION_HOME');
 domain_configuration_home = os.getenv('DOMAIN_CONFIGURATION_HOME');
@@ -9,10 +14,10 @@ fusion_middleware_home = os.getenv('FUSION_MIDDLEWARE_HOME');
 middleware_home = os.getenv('MIDDLEWARE_HOME');
 nodemanager_home = os.getenv('NODE_MANAGER_HOME');
 weblogic_home = os.getenv('WEBLOGIC_HOME');
-
 nodemanager_config_file = domain_configuration_home +'/nodemanager_config_file.properties';
 nodemanager_key_file = domain_configuration_home + '/nodemanager_key_file.properties';
 
+# Connect to Nodemanager to control the AdminServer
 print '[INFO] Connecting to NodeManager..';
 if os.path.isfile(nodemanager_config_file) and os.path.isfile(nodemanager_key_file):
     print "[INFO] Authentication type: 'key_file'";
@@ -24,10 +29,23 @@ else:
     nmConnect(nodemanager_username, nodemanager_password, nodemanager_listen_address, \
               nodemanager_listen_port, domain_name, domain_configuration_home, nodemanager_connection_mode);
 
-print '[INFO] Start AdminServer..';
-nmStart(admin_server_name);
+# NOTE: The server status is logged twice.
+# TODO: Check why nmServerStatus() logs to stdout despite the assignement action below
+server_status = nmServerStatus(admin_server_name);
+print "[DEBUG] Server status is: %s" % server_status
 
+try:
+    if (control_action == 'start') and (server_status != 'RUNNING'):
+        print '[INFO] Starting AdminServer..';
+        nmStart(admin_server_name);
+    elif (control_action == 'stop') and (server_status == 'RUNNING') :
+        print '[INFO] Stopping AdminServer..';
+        nmKill(admin_server_name);
+except:
+    dumpStack();
+    print "[ERROR] Failed to %s the AdminServer !" % control_action
+    exit(defaultAnswer='y', exitcode=1);
+
+# Quit current session
 print '[INFO] Disconnecting from NodeManager..';
 nmDisconnect();
-
-print '[INFO] Done !'
---
+++ files/osb_control_admin_server.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT=$(readlink -f $0)
+SCRIPT_PATH=$(dirname $SCRIPT)
+SUPPORTED_CONTROL_ACTIONS=(stop start)
+
+usage(){
+	echo "[ERROR] Usage: $0 {stop|start}"
+	exit 1
+}
+
+control_admin_server() {
+	${FUSION_MIDDLEWARE_HOME}/common/bin/wlst.sh \
+		-loadProperties ${SCRIPT_PATH}/osb_environment.properties \
+		${SCRIPT_PATH}/osb_control_admin_server.py $CONTROL_ACTION
+}
+
+if [ $# -ne 1 ]; then
+	usage
+fi
+
+if [[ ! ${SUPPORTED_CONTROL_ACTIONS[*]} =~ "$1" ]]; then
+	usage
+else
+	CONTROL_ACTION="$1"
+fi
+
+source ${SCRIPT_PATH}/osb_set_environment_variables.sh
+
+control_admin_server
